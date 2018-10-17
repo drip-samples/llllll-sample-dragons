92051200dbfd909feb16f3d11e5ae3ce27b42916
Ogawa Mitsunori <mitsunori.ogawa@drecom.co.jp>
2018-10-17 17:07:19 +0900

    M: App js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Web3Provider from './providers/Web3Provider'
+import GithubPagesRedirectProvider from './providers/GithubPagesRedirectProvider'
+import ShareTokenDetail from './views/ShareTokenDetail'
 import TokenDetail from './views/TokenDetail'
 import MyPage from './views/MyPage'
 import Header from './components/Header'
@@ -11,15 +13,18 @@ class App extends Component {
   render() {
     return (
       <Web3Provider>
-        <BrowserRouter>
-          <React.Fragment>
-            <Header />
-            <Switch>
-              <Route exact path="/tokens/:tokenId" component={TokenDetail} />
-              <Route exact path="/" component={MyPage} />
-              <Route component={NotFound} />
-            </Switch>
-          </React.Fragment>
+        <BrowserRouter basename={'/llllll-sample-dragons'}>
+          <GithubPagesRedirectProvider>
+            <React.Fragment>
+              <Header />
+              <Switch>
+                <Route exact path="/:networkName/tokens/:tokenId" component={ShareTokenDetail} />
+                <Route exact path="/tokens/:tokenId" component={TokenDetail} />
+                <Route exact path="/" component={MyPage} />
+                <Route component={NotFound} />
+              </Switch>
+            </React.Fragment>
+          </GithubPagesRedirectProvider>
         </BrowserRouter>
       </Web3Provider>
     )
